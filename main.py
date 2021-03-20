a69ffdbb54508c4a330cb092a65f3a64aebfbe8a
Kunwoo-paik <78731386+Kunwoo-paik@users.noreply.github.com>
2021-03-20 18:45:13 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from random import *
+
+
+print("안녕하세요? 반갑습니다 자동복권뽑기 프로그램 입니다")
+
+
+print("작동시작하겠습니다")
+
+
+print("6개의 숫자가 나옵니다")
+
+print(int(random() * 45) +1)
+
+print(int(random() * 45) +1)
+
+print(int(random() * 45) +1)
+
+print(int(random() * 45) +1)
+
+print(int(random() * 45) +1)
+
+print(int(random() * 45) +1)
+
+
